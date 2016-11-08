9ebdb870f4a65c621b9ec68bb7d8532712f664f5
Ata Deniz Aydın <meagtan@users.noreply.github.com>
2016-11-08 21:31:14 +0300

    Implemented musictochords()

--- music.py
+++ music.py
@@ -1,2 +1,19 @@
 # Process music files to be converted into a sequence of chords
 # Requires music21: http://web.mit.edu/music21/
+
+from music21 import *
+
+def musictochords(path):
+    'Open music file at given directory and convert it into a sequence of chords.'
+    try:
+        scor = converter.parse(path).chordify()
+        res = []
+        e, b = pitch.Pitch('E2'), pitch.Pitch('B6')
+        for chor in scor.recurse().getElementsByClass('Chord'):
+            # TODO differentiate tied notes from others so they can be omitted if the search fails otherwise
+            # and include other features such as shifting notes if out of range
+            res.append(tuple(int(p.ps - e.ps) for p in chor.pitches if e <= p <= b)) # only include if within range
+        return res
+    except:
+        pass
+
